41c29b7534f967e6e05c34866d0d4f5d3b579063
crazyhaller <suvigyamadrid@gmail.com>
2023-04-05 21:53:24 +0530

    setup data fetching and routing

---
+++ components/ArticleList.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/jsx-key */
+import ArticleItem from './ArticleItem'
+import articleStyles from '../styles/Article.module.css'
+
+function ArticleList({ articles }) {
+  return (
+    <div className={articleStyles.grid}>
+      {articles.map((article) => (
+        <ArticleItem article={article} />
+      ))}
+    </div>
+  )
+}
+
+export default ArticleList
