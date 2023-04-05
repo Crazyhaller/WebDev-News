9a464947390a3fe175efdec519bf25d3105a9886
crazyhaller <suvigyamadrid@gmail.com>
2023-04-05 22:34:54 +0530

    Final commit, app ready for deployment

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,12 +1,7 @@
-import Head from 'next/head'
-
 function about() {
   return (
     <div>
-      <Head>
-        <title>About</title>
-      </Head>
-      <h1>About</h1>
+      <h1>A beginner project built using Next.js</h1>
     </div>
   )
 }
